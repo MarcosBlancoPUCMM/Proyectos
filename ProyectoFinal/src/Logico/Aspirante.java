5dc97ada75c3da8bd9bc2e34f7b8b9717d8b9507
fatb1 <fatb1@LAPTOP-VQ1D8B7A>
2021-07-27 20:42:26 -0400

    Constructores

--- ProyectoFinal/src/Logico/Aspirante.java
+++ ProyectoFinal/src/Logico/Aspirante.java
@@ -8,4 +8,26 @@ public class Aspirante {
 	protected String nombre, apellidos, ciudad, cedula, telefono, direccion, nacionalidad, pais, correo, sexo, estadoCivil;
 	protected boolean estado; //Empleado o sin trabajo
 	protected int edad;
+	
+	public Aspirante(ArrayList<SolicitudLaboral> solicitud, String nombre, String apellidos, String ciudad,
+			String cedula, String telefono, String direccion, String nacionalidad, String pais, String correo,
+			String sexo, String estadoCivil, boolean estado, int edad) {
+		super();
+		this.solicitud = solicitud;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.ciudad = ciudad;
+		this.cedula = cedula;
+		this.telefono = telefono;
+		this.direccion = direccion;
+		this.nacionalidad = nacionalidad;
+		this.pais = pais;
+		this.correo = correo;
+		this.sexo = sexo;
+		this.estadoCivil = estadoCivil;
+		this.estado = estado;
+		this.edad = edad;
+	}
+	
+	
 }
