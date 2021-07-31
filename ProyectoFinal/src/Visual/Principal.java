d9dd66a8c968ed2c9beeaee00d7636fe7171befa
lissa <lissa@lissa-latitudee7440>
2021-07-31 19:35:07 -0400

    Se puede usar la ventana principal

--- ProyectoFinal/src/Visual/Principal.java
+++ ProyectoFinal/src/Visual/Principal.java
@@ -9,6 +9,8 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JMenu;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Principal extends JFrame {
 
@@ -45,18 +47,46 @@ public class Principal extends JFrame {
 		menuBar.add(mnEmpresa);
 		
 		JMenuItem mntmRegistrarEmpresa = new JMenuItem("Registrar");
+		mntmRegistrarEmpresa.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				RegistrarEmpresa registrarempresa = new RegistrarEmpresa();
+				registrarempresa.setModal(true);
+				registrarempresa.setVisible(true);
+			}
+		});
 		mnEmpresa.add(mntmRegistrarEmpresa);
 		
 		JMenuItem mntmListarEmpresa = new JMenuItem("Listar");
+		mntmListarEmpresa.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ListarEmpresa listarempresa = new ListarEmpresa();
+				listarempresa.setModal(true);
+				listarempresa.setVisible(true);
+			}
+		});
 		mnEmpresa.add(mntmListarEmpresa);
 		
 		JMenu mnAspirante = new JMenu("Aspirante");
 		menuBar.add(mnAspirante);
 		
 		JMenuItem mntmRegistrarAspirante = new JMenuItem("Registrar");
+		mntmRegistrarAspirante.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				RegistrarAspirante registraraspirante = new RegistrarAspirante();
+				registraraspirante.setModal(true);
+				registraraspirante.setVisible(true);
+			}
+		});
 		mnAspirante.add(mntmRegistrarAspirante);
 		
 		JMenuItem mntmListarAspirante = new JMenuItem("Listar");
+		mntmListarAspirante.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ListarAspirante listaraspirante = new ListarAspirante();
+				listaraspirante.setModal(true);
+				listaraspirante.setVisible(true);
+			}
+		});
 		mnAspirante.add(mntmListarAspirante);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
